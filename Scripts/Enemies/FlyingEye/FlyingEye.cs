4ebe14d8ea5bca8ac122c2fa681d0d852ff0faba
OlkiPG <olki4567@gmail.com>
2022-10-03 15:48:30 +0200

    latest commit

--- Scripts/Enemies/FlyingEye/FlyingEye.cs
+++ Scripts/Enemies/FlyingEye/FlyingEye.cs
@@ -6,6 +6,10 @@ public class FlyingEye : MonoBehaviour
 {
     // Attributes
     public float Speed = 3;
+    public float RealisationDistance = 20;
+    public float AttackingDistance = 1;
+    public float AttackingDistanceRange = 1;
+    public float AttackSpeed = 1.5f;
 
     // Components
     private Animator animator;
@@ -14,19 +18,85 @@ public class FlyingEye : MonoBehaviour
     // Player
     private GameObject player;
 
+    // Other
+    private float NextAttack;
+    private float LastXPosition;
+    private int? LastDirection = null;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.Find("Player");
+        NextAttack = Time.time + AttackSpeed;
+        LastXPosition = transform.position.x;
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        rb.MovePosition(Vector2.MoveTowards(transform.position, player.transform.position, Speed*Time.fixedDeltaTime));
-        if(transform.position==player.transform.position){
-                animator.Play("Death");
+        float DistanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if(DistanceToPlayer <= RealisationDistance)
+        {
+            if(DistanceToPlayer >= AttackingDistance-AttackingDistanceRange && DistanceToPlayer <= AttackingDistance+AttackingDistanceRange)
+            {
+                Attack();
+            }
+            else
+            {
+                MoveTowardsPlayer();
+            }
         }
+    }
+
+
+    private void MoveTowardsPlayer()
+    {
+        if(LastXPosition > transform.position.x)    // moving left
+        {
+            if(LastDirection == 1||LastDirection==null)
+            {
+                transform.Rotate(0, 180, 0);
+            }
+            LastDirection = 2;
+        }else if(LastXPosition < transform.position.x)  // moving right
+        {
+            if (LastDirection == 2)
+            {
+                transform.Rotate(0, 180, 0);
+            }
+            LastDirection = 1;
+        }
+
+        LastXPosition = transform.position.x;
+
+        rb.MovePosition(Vector2.MoveTowards(transform.position, player.transform.position, Speed * Time.fixedDeltaTime));
+
+        if (!AnimatorIsPlaying())
+        {
+            animator.Play("Flying");
+        }
+    }
+
+    private void Attack()
+    {
+        if(NextAttack <= Time.time)
+        {
+            animator.Play("Attacking");
+            NextAttack = Time.time + AttackSpeed;
+        }
+        else
+        {
+            if (!AnimatorIsPlaying())
+            {
+                animator.Play("Flying");
+            }
+        }
+    }
+
+    private bool AnimatorIsPlaying()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1;
     }
 }
