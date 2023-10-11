367409ab28a49d985e5e5ba3b6bcca295166e732
OlkiPG <olki4567@gmail.com>
2023-10-11 23:10:43 +0200

    Inventory

---
+++ Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject Inventory;
+    private bool open;
+
+    void Start()
+    {
+        open = false;
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown("r")){
+            if(open){
+                Inventory.SetActive(false);
+                open = false;
+            }else{
+                Inventory.SetActive(true);
+                open = true;
+            }
+        }
+    }
+}
